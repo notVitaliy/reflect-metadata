0b6eb054290221d00d68e95a758236adb41efa82
visikov <visikov@gmail.com>
2016-06-10 10:57:29 -0400

    Add definedMetadata type

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 declare namespace Reflect {
     function getMetadata(metadataKey: any, target: any, propertyKey?: PropertyKey): any;
+    function defineMetadata(metadataKey: any, metadataValue: any, target: Object): void;
 }
